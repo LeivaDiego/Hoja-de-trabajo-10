f9bd1031f418d333293e15d367dfed81d5c7e33f
JPOrellana <orellanaorellanapablo@gmail.com>
2023-05-26 19:40:50 -0600

    Floyd Warshal V1
    Primera version del algoritmo de Floyd Warshal

--- Algoritmo de Floyd/src/FloydWarshall.java
+++ Algoritmo de Floyd/src/FloydWarshall.java
@@ -1,2 +1,86 @@
+/**
+ * Esta clase implementa el algoritmo de Floyd Warshall para calcular las distancias más cortas
+ * entre todas las parejas de ciudades en el grafo.
+ * También proporciona un método para encontrar el centro del grafo y otro para obtener la ruta
+ * más corta entre dos ciudades específicas.
+ *
+ * @author diego leiva, palbo orellana
+ *
+ * Referencia de: malonso-uvg
+ */
 public class FloydWarshall {
+    private int[][] distancias;
+    private String[][] recorridos;
+    private String[] vertices;
+    private int SIZE;
+
+    public FloydWarshall(int [][]_distancias, String[][] _recorridos, int matriz_size) {
+        SIZE = matriz_size;
+        distancias = _distancias;
+        recorridos = _recorridos;
+        vertices = new String[matriz_size];
+        for (int i = 0; i < matriz_size; i++) {
+            vertices[i] = _recorridos[0][i];
+        }
+    }
+
+    /**
+     * @return the distancias
+     */
+    public int[][] getDistancias() {
+        return distancias;
+    }
+
+    /**
+     * @param distancias the distancias to set
+     */
+    public void setDistancias(int[][] distancias) {
+        this.distancias = distancias;
+    }
+
+    /**
+     * @return the recorridos
+     */
+    public String[][] getRecorridos() {
+        return recorridos;
+    }
+
+    /**
+     * @param recorridos the recorridos to set
+     */
+    public void setRecorridos(String[][] recorridos) {
+        this.recorridos = recorridos;
+    }
+
+    /**
+     * @return the sIZE
+     */
+    public int getSIZE() {
+        return SIZE;
+    }
+
+    /**
+     * @param sIZE the sIZE to set
+     */
+    public void setSIZE(int sIZE) {
+        SIZE = sIZE;
+    }
+
+    public void CalcularRutas() {
+        for (int i = 0; i < SIZE; i++) { //Que fila y que columna trabajo
+            for (int j = 0; j < SIZE; j++) {
+                for (int k = 0; k < SIZE; k++) {
+
+                    if ((i != j) && (i != k)) {
+                        int suma = distancias[j][i] + distancias[i][k];
+                        if (suma < distancias[j][k]) {
+                            distancias[j][k] = suma;
+                            recorridos[j][k] = vertices[i];
+                        }
+                    }
+
+                }
+            }
+        }
+    }
 }
--- Algoritmo de Floyd/src/Main.java
+++ Algoritmo de Floyd/src/Main.java
@@ -1,3 +1,8 @@
+/**
+ * Programa que implementa un grafo para aplicar el algoritmo de Floyd
+ * y encontrar la ruta mas corta, el centro del grafo
+ * @author diego leiva, pablo orellana
+ */
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
